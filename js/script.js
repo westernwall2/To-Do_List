4b80cddcfb44931405c2515a4ac36bbba9291df4
westernwall2 <jefersoncosta1994@gmail.com>
2022-07-01 13:12:01 -0300

    To-Do_List 002

--- js/script.js
+++ js/script.js
@@ -5,6 +5,11 @@ const ul = document.querySelector('ul')
 
 var itensDB = []
 
+deletaTudo.onclick = () => {
+    itensDB = []
+    updateDB()
+}
+
 inserir.onclick = () => {
     if(texto.value != '') {
         setItemDB()
@@ -12,3 +17,58 @@ inserir.onclick = () => {
         alert('Digite Algo!')
     }
 }
+function setItemDB() {
+    if (itensDB.length >= 20) {
+        alert('Limite máximo de 20 itens atingido!')
+        return
+    }
+
+    itensDB.push({'item':texto.value, 'status': ''})
+    updateDB()
+}
+function updateDB() {
+    localStorage.setItem('todolist', JSON.stringify(itensDB))
+    loadItens()
+}
+function loadItens() {
+    ul.innerHTML = "";
+    itensDB = JSON.parse(localStorage.getItem('todolist')) ?? []
+    itensDB.forEach((item, i) => {
+        insertItemTela(item.item, item.status, i)
+    })
+}
+function insertItemTela(text, status, i) {
+    const li = document.createElement('li')
+
+    li.innerHTML = `
+    <div class="divLi">
+        <input type="checkbox" ${status} data-i=${i} onchange="done(this, ${i})" />
+        <span data-si=${i}>${text}</span>
+        <button onclick="removeItem(${i})" data-i=${i}>x</>
+    </div>
+    `
+    ul.appendChild(li)
+
+    if (status) {
+        document.querySelector(`[data-si="${i}"]`).classList.add('linha')
+    }else {
+        document.querySelector(`[data-si="${i}"]`).classList.remove('linha')
+    }
+    texto.value = ''
+}
+function done(chk, i) {
+    if (chk.checked) {
+        itensDB[i].status = 'checked'
+    }else {
+        itensDB[i].status = ''
+    }
+
+    updateDB()
+}
+
+function removeItem(i) {
+    itensDB.splice(i, 1)
+    updateDB()
+}
+
+loadItens()
